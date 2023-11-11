0383a430a174a71a94b50cb5633c482e14779ed9
MsLVictor <victormleite25@icloud.com>
2023-11-11 11:10:43 -0300

    first commit

---
+++ script1.js
@@ -0,0 +1,25 @@
+$("#btn").click(function(e){
+    const tarefa = $("#tarefa").val();
+    if(tarefa == ""){
+        alert("Digite uma tarefa válida!");
+    } else {
+        $("#lista").append(`<li>
+                            <i class="fas fa-check-circle check"></i>
+                            <span>${ tarefa }</span>
+                            <i class="fa-solid fa-trash-can close"></i>
+                            </li>` 
+        )
+        $("#tarefa").val("");
+    }
+
+    $(".close").each(function(){
+        $(this).click(function(){
+            $(this).parent().remove();
+        })
+    });
+
+    $("#lista li").click(function(){
+        $(this).find(".check").css("color", "#349223");
+        $(this).find("span").css("text-decoration", "line-through");
+    })
+})
